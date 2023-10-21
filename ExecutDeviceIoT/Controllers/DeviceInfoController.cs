7ff7ae94b922d721de0d6b715fb4126a83c45f82
adef <basc11111@gmail.com>
2023-10-21 19:28:36 +0300

    add

---
+++ ExecutDeviceIoT/Controllers/DeviceInfoController.cs
@@ -0,0 +1,56 @@
+﻿using System.Net;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ExecutingDevice.Controllers;
+
+[ApiController]
+[Route("[controller]/[action]")]
+public class DeviceInfoController : ControllerBase
+{
+    private readonly ILogger<DeviceInfoController> _logger;
+    private readonly IConfiguration _config;
+
+    public DeviceInfoController(ILogger<DeviceInfoController> logger, IConfiguration config)
+    {
+        _logger = logger;
+        _config = config;
+    }
+    
+    [HttpGet(Name = "GetStatus")]
+    public async Task<IActionResult> GetStatus()
+    {
+        var deviceName = _config["DeviceInfo:Name"];
+        var status = _config["DeviceInfo:Status"];
+        
+        _logger.LogInformation($"{DateTime.UtcNow} | Getting status information about the device");
+        return new JsonResult(new {deviceName, status});
+    }
+
+    [HttpPost(Name = "PostChangeStatus")]
+    public async Task<IActionResult> PostChangeStatus(int? value)
+    {
+        var oldStatus = _config["DeviceInfo:Status"];
+        string newStatus; 
+        switch (value)
+        {
+            case 0:
+                newStatus = Status.STOP;
+                break;
+            case 1:
+                newStatus = Status.RUN;
+                break;
+            default:
+                return new JsonResult(HttpStatusCode.BadRequest);
+                break;
+        }
+        
+        if(newStatus.Equals(oldStatus))
+            return new JsonResult(HttpStatusCode.BadRequest);
+        
+        _config["DeviceInfo:Status"] = newStatus;
+        _logger.LogInformation($"{DateTime.UtcNow} | Change status: {oldStatus} --> {newStatus}");
+        
+        return new JsonResult(HttpStatusCode.OK);
+    }
+    
+}
---
+++ ExecutDeviceIoT/Services/TimedHostedService.cs
@@ -0,0 +1,63 @@
+﻿namespace ExecutingDevice.Services;
+
+public class TimedHostedService : IHostedService, IDisposable
+{
+    private int executionCount = 0;
+    private readonly ILogger<TimedHostedService> _logger;
+    private Timer? _timer = null;
+    private readonly IConfiguration _config;
+    public TimedHostedService(ILogger<TimedHostedService> logger, IConfiguration config)
+    {
+        _logger = logger;
+        _config = config;
+    }
+
+    public Task StartAsync(CancellationToken stoppingToken)
+    {
+        _logger.LogInformation("Timed Hosted Service running.");
+
+        _timer = new Timer(DoWork, null, TimeSpan.Zero,TimeSpan.FromSeconds(30));
+
+        return Task.CompletedTask;
+    }
+
+    private void DoWork(object? state)
+    {
+        if (_config["DeviceInfo:Status"].Equals(Status.RUN))
+        {
+            // POST DATA to HEAD CONTROLLER 
+
+            var data = new DeviceData
+            {
+                Name = _config["DeviceInfo:Name"],
+                Data = "Good Day"
+            };
+            JsonContent content = JsonContent.Create(data);
+            HttpClient client = new HttpClient();
+            var response = client.PostAsync("https://localhost:7023/Gateway/PostDeviceData", content);
+            
+            _logger.LogInformation($"{DateTime.UtcNow} | POST data to main device. {response.Result.StatusCode}");
+        }
+           
+    }
+
+    public Task StopAsync(CancellationToken stoppingToken)
+    {
+        _logger.LogInformation("Timed Hosted Service is stopping.");
+
+        _timer?.Change(Timeout.Infinite, 0);
+
+        return Task.CompletedTask;
+    }
+
+    public void Dispose()
+    {
+        _timer?.Dispose();
+    }
+    
+    private class DeviceData
+    {
+        public string Name { get; set; }
+        public string Data { get; set; }
+    }
+}
